0ed169641a675436326e192764dd03f5e339bcb9
Salva <salvador@dosmares.com>
2016-10-05 10:37:21 +0200

    hasta el 6 terminados

--- filterModule.js
+++ filterModule.js
@@ -2,16 +2,18 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (directorio, extension, callback) {
-	var ext = '.' + extension;
 	fs.readdir(directorio, function(error, files) {
+		var ext = '.' + extension;
 		var ficherosFiltrados = Array();
+		if(error){
+			return callback(error);
+		}
 		files.forEach(function(file) {
 			if(ext==path.extname(file)){
 				ficherosFiltrados.push(file);
-			}
-		
-	});
-	callback(null,ficherosFiltrados);
-})
+			}	
+		});
+		callback(null,ficherosFiltrados);
+	})
 	
 }  
